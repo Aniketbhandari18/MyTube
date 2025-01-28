b183ae8bb6303523bafe3e40ec7fca86e8f78656
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-01-28 15:43:30 +0530

    create server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
+import { app } from "./app.js";
+
 dotenv.config()
 
-connectDB();
+connectDB()
+  .then(() =>{
+    app.on("error", (error) =>{
+      console.log("Error running server", error);
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+      console.log(`Server listening at http://localhost:${process.env.PORT}`)
+    });
+  })
+  .catch((error) =>{
+    console.log("MongoDB connection Failed", error);
+  })
