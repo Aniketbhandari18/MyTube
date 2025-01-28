b21e950c6d5d11b8f0268ea35d2f776431d9769f
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-01-29 00:21:28 +0530

    encrypt and decrypt password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import mongoose, { Schema, model } from "mongoose";
 
+import bcryptjs from "bcryptjs"; // for encryption
+import jwt from "jsonwebtoken";
+
 const userSchema = new Schema(
   {
     username: {
@@ -20,7 +23,7 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     fullName: {
       type: String,
@@ -44,12 +47,25 @@ const userSchema = new Schema(
       ],
     },
     refreshToken: {
-      type: String
-    }
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+// encrypt password before saving data
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcryptjs.hash(this.password, 10);
+  next();
+});
+
+//checks if the password is correct
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcryptjs.compare(password, this.password);
+}
 
 export const User = model("User", userSchema);
