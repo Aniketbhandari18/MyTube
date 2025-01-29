439bbbf7865d4f020cf1e64722191c6166dc7d51
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-01-29 15:24:39 +0530

    generate access and refresh token methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -63,9 +63,38 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-//checks if the password is correct
-userSchema.methods.isPasswordCorrect = async function (password){
+//create method to check if the password is correct
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcryptjs.compare(password, this.password);
-}
+};
+
+// create method to generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+//create method to generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = model("User", userSchema);
