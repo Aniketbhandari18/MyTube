f00a070a17339f0a753899fd86cfb89faacc041e
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-02-03 20:37:41 +0530

    add jwt verification middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = async (req, res, next) => {
+  try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized request: Missing token",
+      });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    // add user in req object
+    req.user = user;
+    next();
+
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      message: "Invalid Access Token",
+    });
+  }
+};
