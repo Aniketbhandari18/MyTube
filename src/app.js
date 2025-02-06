34580e06b6082fd8880d0e806dc484b44cee062f
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-02-06 21:09:45 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import { errorHandler } from "./utils/errorHandler.js";
 
 export const app = express();
 
@@ -12,7 +11,3 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 
 app.use("/api/v1/user", userRouter);
-
-
-// handle error middleware
-app.use(errorHandler);
