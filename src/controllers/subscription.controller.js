1254938fa996b2387fe283198911592fcfea8926
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-02-17 21:09:18 +0530

    add getSubscribedChannels controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -50,6 +50,83 @@ const handleSubscription = async (req, res) =>{
   }
 }
 
+const getSubscribedChannels = async (req, res) =>{
+  try {
+    const userId = req.user._id;
+
+    const subscribedChannels = await Subscription.aggregate([
+      {
+        $match: {
+          subscriber: userId
+        }
+      },
+      {
+        $sort: { createdAt: -1 }
+      },
+      // get channel details (_id, username, avatar)
+      {
+        $lookup: {
+          from: "users",
+          localField: "channel",
+          foreignField: "_id",
+          as: "channel",
+          pipeline: [
+            {
+              $project: {
+                "_id": 1,
+                "username": 1,
+                "avatar": 1,
+              }
+            },
+          ]
+        },
+      },
+      {
+        $unwind: "$channel"
+      },
+      // get subscriptions for that channel _id
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "channel._id",
+          foreignField: "channel",
+          as: "channel.subscriptions"
+        }
+      },
+      // count subscribers
+      {
+        $addFields: {
+          "channel.subscriberCount": { $size: "$channel.subscriptions" }
+        }
+      },
+      {
+        $project: {
+          "_id": 0,
+          "channel._id": 1,
+          "channel.username": 1,
+          "channel.avatar": 1,
+          "channel.subscriberCount": 1
+        }
+      },
+      {
+        $replaceRoot: {
+          newRoot: "$channel" // Replace the root with the "channel" object, flattening the structure
+        }
+      }
+    ]);
+  
+    return res.status(200).json({
+      message: "Subscribed channels fetched successfully",
+      totalSubscribedChannels: subscribedChannels.length,
+      subscribedChannels: subscribedChannels
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Error fetching subscribed channels"
+    });
+  }
+};
 
 // Helper functions
 const countSubscribers = (channelId) =>{
@@ -60,4 +137,4 @@ const isSubscribed = (channelId, userId) =>{
   return Subscription.exists({ subscriber: userId, channel: channelId });
 }
 
-export { handleSubscription };
+export { handleSubscription, getSubscribedChannels };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { handleSubscription } from "../controllers/subscription.controller.js";
+import { getSubscribedChannels, handleSubscription } from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 router.route("/c/:channelId").post(verifyJWT, handleSubscription);
+router.route("/").get(verifyJWT, getSubscribedChannels);
 
 export default router;
