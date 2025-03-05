abfe4d428955584096cf7e6eb8d539e949daec0f
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-03-05 21:25:52 +0530

    add: implement utility function for formattedViews

---
+++ frontend/src/utils/number.js
@@ -0,0 +1,10 @@
+const formattedViews = (views) =>{
+  if (views < 1000) return views;
+  if (views < 1000000) return `${Math.floor(views / 1000)}K`;
+  if (views < 1000000000) return `${Math.floor(views / 1000000)}M`;
+  if (views <= 100000000000) return `${Math.floor(views / 1000000000)}B`;
+  
+  return "100B+"
+}
+
+export { formattedViews };
