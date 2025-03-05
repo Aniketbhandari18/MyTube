e7350fb45380d91dee4e878c20fa69643c3d9b1b
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-03-05 18:53:34 +0530

    improve NavBar layout and responsiveness

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,13 +11,13 @@ const NavBar = () => {
   const { user, isAuthenticated } = useAuthStore();
 
   return (
-    <div className="bg-gray-100 flex justify-between py-4 pl-24 pr-6 shadow-md">
-      <div className="shrink-0 flex justify-between items-center">
-        <img className="size-8" src={ videoIcon } />
-        <img className="bg-transparent w-[170px]" src={ logo } />
+    <div className="absolute left-0 right-0 top-0 bg-gray-100 flex justify-between py-4 pl-16 sm:pl-24 pr-6 shadow-md">
+      <div className="hidden sm:flex shrink-0 justify-between items-center">
+        <img className="size-6 md:size-8" src={ videoIcon } />
+        <img className="bg-transparent w-[150px] md:w-[170px]" src={ logo } />
       </div>
 
-      <div className={`relative bg-white rounded-md flex items-center py-2 w-lg shadow-[0_1px_8px_rgb(0,0,0,0.2)] mr-12 ${focus && "shadow-[0_2px_14px_rgb(0,0,0,0.2)]"}`}>
+      <div className={`relative bg-white rounded-md flex items-center py-2 min-w-36 w-lg shadow-[0_1px_8px_rgb(0,0,0,0.2)] ml-4 mr-4 ${focus && "shadow-[0_2px_14px_rgb(0,0,0,0.2)]"}`}>
         <Search className="size-5 absolute left-4 top-2.5 text-gray-500" />
         <input 
           onFocus={() => setFocus(true)} 
@@ -27,7 +27,7 @@ const NavBar = () => {
           placeholder="Search.." />
       </div>
 
-      <div>
+      <div className="shrink-0">
         { 
           isAuthenticated?  user.avatar ?
             <img className="size-10" src="https://cdni.iconscout.com/illustration/premium/thumb/female-user-image-illustration-download-in-svg-png-gif-file-formats--person-girl-business-pack-illustrations-6515859.png?f=webp" />: 
