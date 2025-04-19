4700042b88f23f568ff535a68842beb5d0c43817
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-04-19 23:28:34 +0530

    fix: correct formatting of view counts

--- frontend/src/utils/number.js
+++ frontend/src/utils/number.js
@@ -1,8 +1,8 @@
 const formattedCount = (views) =>{
   if (views < 1000) return views;
-  if (views < 1000000) return `${Math.floor(views / 1000)}K`;
-  if (views < 1000000000) return `${Math.floor(views / 1000000)}M`;
-  if (views <= 100000000000) return `${Math.floor(views / 1000000000)}B`;
+  if (views < 1000000) return `${(views / 1000).toFixed(1)}K`;
+  if (views < 1000000000) return `${(views / 1000000).toFixed(1)}M`;
+  if (views <= 100000000000) return `${(views / 1000000000).toFixed(1)}B`;
   
   return "100B+"
 }
